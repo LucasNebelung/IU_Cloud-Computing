cf2d2c10eea2f05bf85a9fc3589dc849d90743bd
LucasNebelung <lucasnebelung@gmail.com>
2025-02-14 16:24:28 +0100

    updated policy so s3 Bucket can be used as static website

--- main.tf
+++ main.tf
@@ -18,11 +18,28 @@ provider "aws" {
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "iu-test-bucket"
+
+}
+# define aws s3 bucket website config to host static website
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+  index_document { 
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
 }
 
+# upload the index.html file to the S3 bucket
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.website_bucket.id
   key    = "index.html"       # name of the file in S3
   source = "index.html"       #local file
   content_type = "text/html"
 }
+
+##################################################
+# Cloudfront Distribution
+##################################################
+
